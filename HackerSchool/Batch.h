67730e7ee8b6d4bc39700adb42217a847f57bb4d
Adam Fraser <adamfraser0@gmail.com>
2013-02-14 16:07:32 -0500

    Add batch data model

--- HackerSchool/Batch.h
+++ HackerSchool/Batch.h
@@ -1,11 +1,3 @@
-//
-//  Batch.h
-//  HackerSchool
-//
-//  Created by Adam Fraser on 14/02/13.
-//  Copyright (c) 2013 Adam Fraser. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
